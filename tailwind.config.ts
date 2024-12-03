f88468bb53b6f2e508b1789e908d54c165f8e3cf
amanfangeria980 <amanfangeria980@gmail.com>
2024-12-03 22:44:29 +0530

    feat: add primary and secondary color to tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#555770",
+        secondary: "#ffffff",
       },
     },
   },
