76b2d5b0a7bdf1b1d6a340990d4cc2f60f68ee48
Parth Agarwal <parth1001.be21@chitkara.edu.in>
2025-01-05 14:25:41 +0530

    added the two pointer status feature

--- src/app/admin/two-pointer-status/page.tsx
+++ src/app/admin/two-pointer-status/page.tsx
@@ -5,9 +5,16 @@
 // 3. Use logic .ts to create the two pointer status
 
 
-// Next action => You have got you need from database , now write logic for diplaying the two pointer status on the front end using the logic.js file in aman's code 
+// Next action => Check Status 
 "use client"
+
+import { useState } from "react"
 const Page = ()=>{
+
+    const [day, setDay] = useState<string>("")
+    const [finStatus, setFinStatus]=useState<any>([])
+
+   
 
 
 
@@ -16,15 +23,61 @@ const Page = ()=>{
         const sendData = {
 
             username : "parth",
-            day : 3
+            day : parseInt(day,10)
     
         }
         const response = await fetch('http://localhost:5173/get-two-pointer-status', {method : "POST", headers : {'Content-Type' : 'application/json'}, body : JSON.stringify(sendData)})
         const repData = await response.json()
-        const {dayYesterday, dayBeforeYesterday} = repData ; 
+        console.log("The message from the backend ", repData)
+        const results = repData.results ; 
+        if(results)
+        {
+            const {dayYesterday, dayBeforeYesterday} = repData.data ; 
 
-        console.log("The value of dayYesterday is ", dayYesterday) ; 
-        console.log("The value of dayBeforeYesterday is ", dayBeforeYesterday)
+            console.log("The value of dayYesterday is ", dayYesterday) ; 
+            console.log("The value of dayBeforeYesterday is ", dayBeforeYesterday)
+
+            const checkStatus = (dayYesterday : String, dayBeforeYesterday : String)=>{
+                let status = ""
+
+                if( dayYesterday === 'gateway' && dayBeforeYesterday === 'gateway')
+                {
+                    status = "duck"
+                }
+                else if(dayYesterday === 'gateway' && dayBeforeYesterday === 'no')
+                {
+                    status = "duck"
+                }
+                else if(dayYesterday === 'no' && dayBeforeYesterday === 'gateway')
+                {
+                    status = "crab"
+                }
+                else if(dayYesterday === 'no' && dayBeforeYesterday === 'no')
+                {
+                    status = "cross"
+                }
+
+                return status 
+
+            }
+
+            const status = checkStatus(dayYesterday, dayBeforeYesterday)
+
+            let pushValue = {
+                username : "parth", 
+                status : status
+            }
+
+            setFinStatus((prevFinStatus)=>[...prevFinStatus,pushValue])
+           
+            
+
+        }
+        else
+        {
+            console.log(repData.message)
+        }
+        
 
         
         
@@ -33,7 +86,22 @@ const Page = ()=>{
         <>
             <div>This is the admin page for generating 2 pointer status </div>
             <div>
-                <button onClick={fetchTwoPointerStatus}>Generate two pointer status </button>
+                <h2>Date : </h2>
+                
+            </div>
+            {day}
+            
+            <div>
+                <input type="number" className="border-2 border-black bg-gray" min={1} max={25} value={day} onChange={(e)=> setDay(e.target.value)} />
+                <button onClick={fetchTwoPointerStatus} className="border-2 border-black bg-gray-200 disabled:bg-red-400" disabled={!day || isNaN(parseInt(day, 10))}>Generate two pointer status </button>
+
+            </div>
+
+            <div>
+                <h2>This is the finstatus</h2>
+                {
+                    JSON.stringify(finStatus)
+                }
             </div>
         
         
